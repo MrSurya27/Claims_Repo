0ef8d03b6d285e753dced32d146523e5f96e595a
Surya <s.surya2727@gmail.com>
2024-02-28 17:22:05 +0530

    Contains the error handling file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-//const errorHandler = require("./middleware/errorhandler");
+const errorHandler = require("./middleware/errorhandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/users",require("./routes/userRoute"));
-//app.use(errorHandler)
+app.use(errorHandler)
 
 app.listen(port,()=> {
     console.log(`Server running on port ${port}`);
